423dd9300061b6a8093157ffbcf820d10721af49
iqbalzayn01 <iqbalzayyanabiyyu@gmail.com>
2025-07-18 16:51:19 +0700

    fix: hero section

--- src/app/(index)/components/HeroSection.tsx
+++ src/app/(index)/components/HeroSection.tsx
@@ -1,105 +1,79 @@
-import { LayoutGrid } from '@/components/ui/layout-grid';
+import { Button } from '@/components/ui/button';
 import React from 'react';
 import Image from 'next/image';
-
-const SkeletonOne = () => {
-  return (
-    <div className="flex flex-col px-12 py-14 bg-highlight rounded-3xl">
-      <h4 className="font-serif text-[41px] md:text-[46px] text-foreground">
-        Sourdough, butter-soft and golden.
-      </h4>
-      <Image
-        src="https://olxwzgxszqzlanhykudq.supabase.co/storage/v1/object/public/products/product/hero-img-1.jpg"
-        alt="Sourdough bread"
-        width={800}
-        height={800}
-        priority
-        className="size-full rounded-3xl"
-      />
-    </div>
-  );
-};
-const SkeletonTwo = () => {
-  return (
-    <div className="flex flex-col px-12 py-14 bg-highlight rounded-3xl">
-      <h4 className="font-serif text-[41px] md:text-[46px] text-foreground">
-        Sourdough, butter-soft and golden.
-      </h4>
-      <Image
-        src="https://olxwzgxszqzlanhykudq.supabase.co/storage/v1/object/public/products/product/hero-img-1.jpg"
-        alt="Sourdough bread"
-        width={800}
-        height={800}
-        priority
-        className="size-full rounded-3xl"
-      />
-    </div>
-  );
-};
-const SkeletonThree = () => {
-  return (
-    <div className="flex flex-col px-12 py-14 bg-highlight rounded-3xl">
-      <h4 className="font-serif text-[41px] md:text-[46px] text-foreground">
-        Sourdough, butter-soft and golden.
-      </h4>
-      <Image
-        src="https://olxwzgxszqzlanhykudq.supabase.co/storage/v1/object/public/products/product/hero-img-2.jpg"
-        alt="Sourdough bread"
-        width={800}
-        height={800}
-        priority
-        className="size-full rounded-3xl"
-      />
-    </div>
-  );
-};
-const SkeletonFour = () => {
-  return (
-    <div className="flex flex-col px-12 py-14 bg-highlight rounded-3xl">
-      <h4 className="font-serif text-[41px] md:text-[46px] text-foreground">
-        Sourdough, butter-soft and golden.
-      </h4>
-    </div>
-  );
-};
-
-const cards = [
-  {
-    id: 1,
-    content: <SkeletonOne />,
-    className: 'md:col-span-4 md:row-span-3',
-  },
-  {
-    id: 2,
-    content: <SkeletonTwo />,
-    className: 'col-span-4 md:row-span-2',
-  },
-  {
-    id: 3,
-    content: <SkeletonThree />,
-    className: 'col-span-4 md:row-span-1',
-  },
-  {
-    id: 4,
-    content: <SkeletonFour />,
-    className: 'md:col-span-4 md:row-span-3',
-  },
-];
 
 export default function HeroSection() {
   return (
     <div className="container mx-auto px-6 md:px-10 xl:px-24 py-6 flex">
-      <div className="flex w-full items-start justify-between gap-6">
+      <div className="flex flex-col lg:flex-row w-full items-start justify-between gap-4s">
         <div className="flex-1 flex flex-col gap-4">
-          <h1 className="font-serif text-[46px] text-foreground font-bold">
+          <h1 className="text-diplay-xl-emphasized text-foreground">
             Calm mornings. Warm ovens. Real bread.
           </h1>
-          <p className="font-sans text-lg text-foreground">
+          <p className="text-body-md text-foreground">
             Ovenelle brings timeless baking into modern hands.
           </p>
         </div>
-        <div className="w-[62%] h-screen">
-          <LayoutGrid cards={cards} />
+        <div className="w-full lg:w-[65%] h-screen">
+          <div className="grid gap-2 grid-cols-1 sm:grid-cols-4 md:grid-rows-3">
+            <div className="bg-highlight rounded-3xl px-10 py-8 md:px-12 md:py-14 sm:col-span-2 sm:row-span-2 flex flex-col justify-between gap-4">
+              <h2 className="text-display-xl text-foreground">
+                Sourdough, butter-soft and golden.
+              </h2>
+              <Image
+                src="https://olxwzgxszqzlanhykudq.supabase.co/storage/v1/object/public/products/product/hero-img-1.jpg"
+                alt="Sourdough bread"
+                width={500}
+                height={500}
+                priority
+                className="w-full h-auto object-cover flex-1 rounded-3xl"
+              />
+            </div>
+
+            <div className="bg-highlight rounded-3xl px-10 py-8 md:px-12 md:py-14 sm:col-span-2 flex flex-col gap-4">
+              <h3 className="text-heading-md-emphasized text-foreground">
+                Discover what&apos;s fresh today
+              </h3>
+              <p className="text-caption text-foreground">
+                Every loaf tells a story.
+              </p>
+              <Button className="bg-foreground w-fit px-6 py-[9.5px] rounded-md text-caption text-surface hover:bg-black">
+                Order Now
+              </Button>
+            </div>
+
+            <div className="bg-highlight rounded-3xl px-10 py-8 md:px-12 md:py-14 sm:col-span-2 sm:row-span-2 flex flex-col justify-between gap-4">
+              <Image
+                src="https://olxwzgxszqzlanhykudq.supabase.co/storage/v1/object/public/products/product/hero-img-2.jpg"
+                alt="Sourdough bread"
+                width={500}
+                height={500}
+                priority
+                className="size-full object-cover rounded-3xl"
+              />
+              <h3 className="text-heading-md-emphasized text-foreground mb-2">
+                Honest baking, no shortcuts
+              </h3>
+              <p className="text-body-sm text-foreground">
+                Minimal ingredients, maximum comfort.
+              </p>
+            </div>
+
+            <div className="bg-highlight rounded-3xl px-10 py-8 md:px-12 md:py-14 sm:col-span-2 flex flex-col gap-4">
+              <h3 className="text-heading-md-emphasized text-foreground">
+                Open Daily
+              </h3>
+              <p className="text-body-md text-foreground">
+                7 AM - 6 PM • Bogor, Indonesia
+              </p>
+              <Button
+                variant="outline"
+                className="w-fit cursor-pointer bg-transparent hover:bg-foreground border border-foreground py-2 px-4 rounded-md text-caption text-foreground hover:text-surface"
+              >
+                Find Us
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,14 +1,19 @@
 import { Button } from '../ui/button';
+import Link from 'next/link';
 import React from 'react';
 
 export default function Header() {
   return (
     <div className="container mx-auto px-6 py-6 md:10 xl:px-24 flex items-center">
       <div className="w-full flex items-center justify-between">
-        <h1 className="font-serif text-4xl text-brand font-bold">Ovenelle</h1>
+        <Link href="/" className="text-heading-xl-emphasized text-brand">
+          Ovenelle
+        </Link>
         <div className="flex items-center justify-end gap-10">
           <div className="flex items-start gap-2">
-            <span className="font-sans text-sm text-foreground">Cart</span>
+            <Link href="/cart" className="text-caption text-foreground">
+              Cart
+            </Link>
             <svg
               width="8"
               height="8"
@@ -25,7 +30,7 @@ export default function Header() {
               height="24px"
               viewBox="0 -960 960 960"
               width="24px"
-              className="fill-base"
+              className="fill-surface"
             >
               <path d="M400-240q-17 0-28.5-11.5T360-280q0-17 11.5-28.5T400-320h400q17 0 28.5 11.5T840-280q0 17-11.5 28.5T800-240H400Zm0-200q-17 0-28.5-11.5T360-480q0-17 11.5-28.5T400-520h400q17 0 28.5 11.5T840-480q0 17-11.5 28.5T800-440H400ZM160-640q-17 0-28.5-11.5T120-680q0-17 11.5-28.5T160-720h640q17 0 28.5 11.5T840-680q0 17-11.5 28.5T800-640H160Z" />
             </svg>
